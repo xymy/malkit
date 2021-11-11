d6b15611088ab1c035c7666ac002ac4d4cdcf563
xymy <thyfan@163.com>
2021-11-11 09:40:07 +0800

    Add categorize_folder

--- src/malkit/__init__.py
+++ src/malkit/__init__.py
@@ -1,11 +1,12 @@
 from .image import convert_binary_to_image, convert_binary_to_image_parallel, resize_image, resize_image_parallel
-from .utils import convert_bytes_to_binary, convert_bytes_to_binary_parallel
+from .utils import categorize_folder, convert_bytes_to_binary, convert_bytes_to_binary_parallel
 
 __all__ = [
     "convert_binary_to_image",
     "convert_binary_to_image_parallel",
     "resize_image",
     "resize_image_parallel",
+    "categorize_folder",
     "convert_bytes_to_binary",
     "convert_bytes_to_binary_parallel",
 ]
--- src/malkit/utils.py
+++ src/malkit/utils.py
@@ -1,10 +1,32 @@
 import functools
+from pathlib import Path
 from typing import Any, Iterable, Optional
+
+import pandas as pd
 
 from ._parallel import execute_parallel
 from ._typing import FilePath
 
-__all__ = ["convert_bytes_to_binary", "convert_bytes_to_binary_parallel"]
+__all__ = ["categorize_folder", "convert_bytes_to_binary", "convert_bytes_to_binary_parallel"]
+
+
+def categorize_folder(root: FilePath, labels: pd.DataFrame, *, suffix: Optional[str] = None) -> bool:
+    root = Path(root)
+    sample_names = [str(item) for item in labels.iloc[:, 0]]
+    target_names = [str(item) for item in labels.iloc[:, 1]]
+
+    for klass in set(target_names):
+        cat_dir = root / klass
+        if cat_dir.is_dir():
+            return
+        cat_dir.mkdir(parents=True, exist_ok=True)
+
+    for sample_name, target_name in zip(sample_names, target_names):
+        src = root / sample_name
+        if suffix is not None:
+            src = src.with_suffix(suffix)
+        dst = root / target_name / sample_name
+        src.rename(dst)
 
 
 def convert_bytes_to_binary(bytes_file: FilePath, binary_file: FilePath, *, qq: str = "00") -> None:
