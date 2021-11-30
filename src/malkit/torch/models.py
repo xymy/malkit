c36566283757e73c581097cbbcad0d035a77589e
xymy <thyfan@163.com>
2021-11-30 20:07:27 +0800

    Update MalConv

--- src/malkit/torch/models.py
+++ src/malkit/torch/models.py
@@ -7,7 +7,7 @@ class MalConv(nn.Module):
 
     References:
         - Edward Raff et al. 2018. Malware Detection by Eating a Whole EXE.
-          https://www.aaai.org/ocs/index.php/WS/AAAIW18/paper/view/16422
+          https://arxiv.org/abs/1710.09435
     """
 
     def __init__(self, num_classes: int = 2) -> None:
@@ -16,16 +16,12 @@ class MalConv(nn.Module):
         # 257 = byte (0-255) + padding (256)
         self.embedding = nn.Embedding(257, 8)
 
-        self.conv1 = nn.Conv1d(8, 128, kernel_size=500, stride=500, bias=True)
-        self.conv2 = nn.Conv1d(8, 128, kernel_size=500, stride=500, bias=True)
+        self.conv1 = nn.Conv1d(8, 128, kernel_size=500, stride=500)
+        self.conv2 = nn.Conv1d(8, 128, kernel_size=500, stride=500)
+        self.relu = nn.ReLU(inplace=True)
 
         self.maxpool = nn.AdaptiveMaxPool1d(1)
-        self.fc = nn.Sequential(
-            nn.Flatten(),
-            nn.Linear(128, 128),
-            nn.ReLU(inplace=True),
-            nn.Linear(128, num_classes),
-        )
+        self.fc = nn.Linear(128, num_classes)
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
         x = self.embedding(x)
@@ -35,7 +31,9 @@ class MalConv(nn.Module):
 
         # Perform gated convolution.
         x = self.conv1(x) * torch.sigmoid(self.conv2(x))
+        x = self.relu(x)
 
         x = self.maxpool(x)
-        x = self.fc(x)
+        x = self.fc(torch.flatten(x, 1))
+
         return x
