f21fee0046db1cd3d04bd5373099065ce3d00be5
xymy <thyfan@163.com>
2021-12-24 10:39:19 +0800

    Add byte entropy histogram

--- src/malkit/byte.py
+++ src/malkit/byte.py
@@ -1,11 +1,17 @@
-from typing import Any, Iterable, Optional
+import functools
+from typing import Any, Iterable, Optional, Tuple
 
 import numpy as np
 
 from ._parallel import reduce_parallel
 from ._typing import FilePath
 
-__all__ = ["extract_byte_hist", "extract_byte_hist_parallel"]
+__all__ = [
+    "extract_byte_hist",
+    "extract_byte_hist_parallel",
+    "extract_byte_entropy_hist",
+    "extract_byte_entropy_hist_parallel",
+]
 
 
 def get_byte_hist(buffer: bytes) -> np.ndarray:
@@ -29,3 +35,45 @@ def extract_byte_hist_parallel(
     """Extract byte histogram in parallel."""
 
     return reduce_parallel(extract_byte_hist, binary_files, n_jobs=n_jobs, **kwargs)
+
+
+def _get_entropy_and_byte_hist(window: np.ndarray) -> Tuple[np.float64, np.ndarray]:
+    byte_hist = np.bincount(window, minlength=256)
+    prob = byte_hist / len(window)
+    cond = prob > 0
+    entropy = -np.sum(prob * np.log2(prob, where=cond))
+    return entropy, byte_hist
+
+
+def get_byte_entropy_hist(buffer: bytes, *, window_size: int = 1024, step_size: int = 256) -> np.ndarray:
+    """Get byte entropy histogram."""
+
+    byte_seq = np.frombuffer(buffer, dtype=np.uint8)
+    result = np.zeros((16, 256), dtype=np.int64)
+    for window in np.lib.stride_tricks.sliding_window_view(byte_seq, window_size)[::step_size]:
+        entropy, byte_hist = _get_entropy_and_byte_hist(window)
+        idx = min(int(entropy * 2), 15)
+        result[idx] += byte_hist
+    return result.reshape((16, 16, 16)).sum(-1).reshape(256)
+
+
+def extract_byte_entropy_hist(binary_file: FilePath, *, window_size: int = 1024, step_size: int = 256) -> np.ndarray:
+    """Extract byte entropy histogram."""
+
+    with open(binary_file, "rb") as f:
+        buffer = f.read()
+    return get_byte_entropy_hist(buffer, window_size=window_size, step_size=step_size)
+
+
+def extract_byte_entropy_hist_parallel(
+    binary_files: Iterable[FilePath],
+    *,
+    window_size: int = 1024,
+    step_size: int = 256,
+    n_jobs: Optional[int] = None,
+    **kwargs: Any,
+) -> np.ndarray:
+    """Extract byte entropy histogram in parallel."""
+
+    function = functools.partial(extract_byte_entropy_hist, window_size=window_size, step_size=step_size)
+    return reduce_parallel(function, binary_files, n_jobs=n_jobs, **kwargs)
