e5c1b5c75d6c4bb2420edee338173cc7f15398c8
xymy <thyfan@163.com>
2022-04-18 09:48:55 +0800

    Update loader

--- src/malkit/torch/data/_loader.py
+++ src/malkit/torch/data/_loader.py
@@ -1,11 +1,18 @@
 from typing import Tuple
 
+from torch import Tensor
+
+from ..._typing import FilePath
+
 
 class Loader:
+    def __call__(self, path: FilePath) -> Tensor:
+        raise NotImplementedError
+
     def __repr__(self) -> str:
         args = self._get_args()
         args_str = ", ".join(f"{a}={getattr(self, a)!r}" for a in args)
         return f"{type(self).__name__}({args_str})"
 
     def _get_args(self) -> Tuple[str, ...]:
-        return ()
+        raise NotImplementedError
