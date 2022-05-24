0fed428cba7a97ac638a961f9049aeb986b48f90
xymy <thyfan@163.com>
2022-05-24 10:03:33 +0800

    Move malconv to models/

--- src/malkit/torch/models/malconv.py
+++ src/malkit/torch/models/malconv.py
@@ -2,6 +2,7 @@ from typing import Optional
 
 import torch
 import torch.nn as nn
+from torch import Tensor
 
 
 class MalConv(nn.Module):
@@ -25,6 +26,8 @@ class MalConv(nn.Module):
     ) -> None:
         super().__init__()
 
+        self.num_classes = num_classes
+
         # By default, num_embeddings (257) = byte (0-255) + padding (256).
         self.embedding = nn.Embedding(num_embeddings, embedding_dim, padding_idx=padding_idx)
 
@@ -40,16 +43,25 @@ class MalConv(nn.Module):
             nn.Linear(channels, num_classes),
         )
 
-    def forward(self, x: torch.Tensor) -> torch.Tensor:
+    def _embed(self, x: Tensor) -> Tensor:
+        # Perform embedding.
         x = self.embedding(x)
 
         # Treat embedding dimension as channel.
         x = x.permute(0, 2, 1)
+        return x
 
+    def _forward_embedded(self, x: Tensor) -> Tensor:
         # Perform gated convolution.
         x = self.conv1(x) * torch.sigmoid(self.conv2(x))
 
+        # Perform global max pooling.
         x = self.maxpool(x)
 
         x = self.fc(x)
+        return x
+
+    def forward(self, x: Tensor) -> Tensor:
+        x = self._embed(x)
+        x = self._forward_embedded(x)
         return x
